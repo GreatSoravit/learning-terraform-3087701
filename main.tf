373003085fadf508346a53798d3080386d72e2dd
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-06-21 18:42:34 +0700

    test

--- main.tf
+++ main.tf
@@ -2,8 +2,13 @@ data "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
-    name   = "virtualization-type"
+    name   = "name"
     values = [var.ami_filter.name]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 
   owners = [var.ami_filter.owner]

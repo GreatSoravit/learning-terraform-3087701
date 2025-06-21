841789b113bc373591ea5c2ab1d64edc5173f6e3
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-06-21 17:37:44 +0700

    aws

--- providers.tf
+++ providers.tf
@@ -1,10 +1,16 @@
+generate "required_providers" {
+  path      = "versions.tf"
+  if_exists = "overwrite"
+  contents = <<EOF
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "< 6.0"
+      version = "< 6.0.0"
     }
   }
+}
+EOF
 }
 
 provider "aws" {

99b78d734bccdf79aa85488a765e35e33c25912f
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-06-21 17:36:37 +0700

    terra

--- providers.tf
+++ providers.tf
@@ -1,18 +1,11 @@
-generate "versions_override" {
-  path      = "versions_override.tf"
-  if_exists = "overwrite_terragrunt"
-  contents  = <<EOF
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "< 6.0.0"
+      version = "< 6.0"
     }
   }
 }
-EOF
-}
-
 
 provider "aws" {
   region  = "us-west-2"

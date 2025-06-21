cbb1b01380c34fed63d11636832fb95231a79e83
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-06-21 17:35:21 +0700

    overrider

--- providers.tf
+++ providers.tf
@@ -1,10 +1,16 @@
+generate "versions_override" {
+  path      = "versions_override.tf"
+  if_exists = "overwrite_terragrunt"
+  contents  = <<EOF
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "< 6.0.0"
     }
   }
+}
+EOF
 }
 
 
